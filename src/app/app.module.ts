61d1311c38a996e603fa1806a5251cd137850642
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-20 02:07:21 +0300

    imported components: search-users, and search-repos such that they are accessible globally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { SearchUsersComponent } from './search-users/search-users.component';
+import { SearchReposComponent } from './search-repos/search-repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    SearchUsersComponent,
+    SearchReposComponent
   ],
   imports: [
     BrowserModule,
