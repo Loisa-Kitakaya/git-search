75b0776dfda5c41afc778c51b43c533d88163ba0
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-20 03:32:01 +0300

    imported the HttpClientModule and the RoutingModule (the latter exported by the custom routing module); to access their functionallity globally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
 import { SearchUsersComponent } from "./search-users/search-users.component";
 import { SearchReposComponent } from "./search-repos/search-repos.component";
-
-const appRoutes: Routes = [
-  { path: "repos", component: SearchReposComponent },
-  { path: "users", component: SearchUsersComponent },
-  { path: "welcome", component: WelcomePageComponent },
-  { path: "", redirectTo: "/welcome", pathMatch: "full" }
-];
+import { RoutingModule } from './routing/routing/routing.module';
 
 @NgModule({
   declarations: [
@@ -27,10 +21,8 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    RoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
