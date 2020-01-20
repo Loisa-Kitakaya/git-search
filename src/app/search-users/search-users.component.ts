91a7e10a86acf53a84782bc5903bb98acbeaf838
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-20 09:45:29 +0300

    created an object: user, that inherits its properties from the user-information class; created a function that injects the get-user-service; on page load, displays my information, but as the user types in input, show related data (achieved by using angular component lifecycles)

--- src/app/search-users/search-users.component.ts
+++ src/app/search-users/search-users.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GetUsersService } from "../get-users-service/get-users.service";
+import { UserInformation } from "../users/user-information";
 
 @Component({
-  selector: 'app-search-users',
-  templateUrl: './search-users.component.html',
-  styleUrls: ['./search-users.component.css']
+  selector: "app-search-users",
+  templateUrl: "./search-users.component.html",
+  styleUrls: ["./search-users.component.css"],
+  providers: [GetUsersService]
 })
 export class SearchUsersComponent implements OnInit {
+  user: UserInformation;
+  username: string;
 
-  constructor() { }
+  constructor(private requestUser: GetUsersService) {}
 
-  ngOnInit() {
+  // function that injects the user service
+  getName() {
+    this.requestUser.getName(this.username);
+    this.requestUser.getRequest();
+    this.user = this.requestUser.user;
   }
 
+  // on page load fetch this
+  ngOnInit() {
+    this.requestUser.username = "Loisa-Kitakaya";
+    this.requestUser.getRequest();
+    this.user = this.requestUser.user;
+  }
+
+  //on any changes to the DOM properties, fetch this
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    this.getName;
+  }
 }
