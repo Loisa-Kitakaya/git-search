e4487c8bab1defa2810970c42c183bb331c46066
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-20 14:47:51 +0300

    created a method of class RepoArray, that stores the api-request

--- src/app/get-repos-service/get-repos.service.ts
+++ src/app/get-repos-service/get-repos.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { RepoArray } from "../repos/repo-array";
+
+//constants that hold the git api url and the api access tokens
+const apiLink = `${environment.baseUrl}`;
+const apiToken = `${environment.accessTokenRepo}`;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GetReposService {
+  repoarray: RepoArray;
+  username: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.repoarray = new RepoArray("");
+  }
+
+  //function to get user input
+  getName(username: string) {
+    this.username = username;
+  }
+
+  //values to fetch
+  getRequest() {
+    interface ApiRequest {
+      html_url: any;
+      name: string;
+      description: any;
+      forks: any;
+      license: any;
+    }
+    //make api request
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get<ApiRequest>(apiLink + this.username + apiToken)
+        .toPromise()
+        .then(
+          response => {
+            this.repoarray.repoArray = response;
+
+            resolve();
+          },
+          error => {
+            this.repoarray.repoArray = [];
+
+            reject(error);
+          }
+        );
+    });
+    return promise;
+  }
 }
