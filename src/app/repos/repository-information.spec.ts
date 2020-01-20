dc0d516933fa46c7e10e8af6bcd9d08fe27be894
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-20 03:33:02 +0300

    created a class RpositoryInformation

---
+++ src/app/repos/repository-information.spec.ts
@@ -0,0 +1,7 @@
+import { RepositoryInformation } from './repository-information';
+
+describe('RepositoryInformation', () => {
+  it('should create an instance', () => {
+    expect(new RepositoryInformation()).toBeTruthy();
+  });
+});
