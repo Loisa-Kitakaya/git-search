0b494bf462761f345b6eefa3399268fda44ec0ab
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-20 04:13:00 +0300

    created a constructor to specify properties of the objects to be created

--- src/app/users/user-information.ts
+++ src/app/users/user-information.ts
@@ -1,2 +1,14 @@
 export class UserInformation {
+  //constructor
+  constructor(
+    public avatar: any,
+    public userName: string,
+    public name: string,
+    public bio: any,
+    public repositories: any,
+    public followers: any,
+    public following: any,
+    public link: any,
+    public date: Date
+  ) {}
 }
