d60ba58df8e63d5d41f780313ace4cc6a51c5d21
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-20 04:12:47 +0300

    created a constructor to specify properties of the objects to be created

--- src/app/repos/repository-information.ts
+++ src/app/repos/repository-information.ts
@@ -1,2 +1,10 @@
 export class RepositoryInformation {
+  //constructor
+  constructor(
+    public link: any,
+    public name: string,
+    public description: any,
+    public forks: any,
+    public license: any
+  ) {}
 }
