e6f883e4ab4059d435d3b307350cf0713946f550
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-21 12:16:05 +0300

    defined the behaviour of the pipe

--- src/app/custom-date.pipe.ts
+++ src/app/custom-date.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CustomDatePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    return null;
+    return this.transform(value, "EEEE d MMMM y h:mm a");
   }
 
 }
